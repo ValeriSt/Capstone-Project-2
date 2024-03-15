89053f396290afbc4c423da4bbfdbc371ab2053d
ValeriSt <valst1@abv.bg>
2024-03-15 13:13:46 +0200

    Change footer to mail and github icons for a hyperlink

--- index.js
+++ index.js
@@ -11,11 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 3000;
 const publications = [];
-// let currentCity = ""; // Initialize variable to store current city
 
 
 app.use(express.static("public"));
-// app.use(checkCurrentURL);
 
 app.get("/", (req, res) => {
     res.render("index.ejs", { publications: publications });
@@ -50,42 +48,6 @@ app.post("/submit", (req, res) => {
         res.redirect(`/${country}`)
     }
 });
-
-// function checkCurrentURL(req, res, next) {
-//     // Determine the current page URL
-//     const currentPageURL = `${req.originalUrl}`;
-
-//     // You can perform any checks or actions based on the current URL here
-//     if (currentPageURL === "/") {
-//         // If the current page is the home page
-//         currentCity = ""; // Reset current city when accessing the home page
-//     } else if (currentPageURL === "/Rome") {
-//         // If the current page is Rome
-//         currentCity = "Rome";
-//     } else if (currentPageURL === "/Paris") {
-//         app.get("Paris/publication", (req, res, next) => {
-//             if (currentCity == "Paris") {
-//                 res.render("publication.ejs");
-//             } else {
-//                 // Otherwise, render default publication page
-//                 next();
-//             }
-//         });
-//     } else if (currentPageURL === "/Barcelona") {
-//         // If the current page is Barcelona
-//         currentCity = "Barcelona";
-//     } 
-    
-//     // Call the next middleware function in the stack
-//     next();
-// }
-
-
-
-// Register the checkCurrentURL middleware to be used for all routes
-// app.use(checkCurrentURL);
-
-
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
